fea47b22f03a0ef8a51451dcf9cb51bff421f7bc
RomanRabinovich <roman.kliakhin@gmail.com>
2022-07-21 21:49:46 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/seamens_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.seamens_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+//import fr pack
+const express = require("express");
+const mongoose = require('mongoose');
+
+//import fr other
+const authRouter = require('./routes/auth');
+
+//init
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://roman:zccclDQ8DvtJx0LU@cluster0.e75jo.mongodb.net/?retryWrites=true&w=majority"
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+//connection
+mongoose.connect(DB).then(() => {
+    console.log('Connection Successful');
+}).catch((e) => {
+    console.log(e);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+});
