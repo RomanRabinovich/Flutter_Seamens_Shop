371335031054b616cb5727f3581cdd1110309ea1
RomanRabinovich <roman.kliakhin@gmail.com>
2022-07-24 15:51:08 +0300

    product

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,12 +4,25 @@ const auth = require('../middlewares/auth');
 const Product = require("../models/product");
 
 productRouter.get("/api/products/", auth, async (req, res) => {
-  try {
-    console.log(req.query.category); 
+  try { 
     const products = await Product.find({ category: req.query.category });
     res.json(products);
   } catch (e) {
     res.status(500).json({ error: e.message }); 
+  }
+});
+
+// create a get request to search products and get them
+// /api/products/search/i
+productRouter.get("/api/products/search/:name", auth, async (req, res) => {
+  try {
+    const products = await Product.find({
+      name: { $regex: req.params.name, $options: "i" },
+    });
+
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
