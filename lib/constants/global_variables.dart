c8edae5f8c6c65e322ccdf0253746848546607ef
RomanRabinovich <roman.kliakhin@gmail.com>
2022-08-04 12:43:22 +0300

    done

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.118.128:3000';
+String uri = 'https://seamens-club-shop.heroku.com:3000';
 
 class GlobalVariables {
   // COLORS
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
 //init
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const DB = "mongodb+srv://roman:zccclDQ8DvtJx0LU@cluster0.e75jo.mongodb.net/?retryWrites=true&w=majority"
 
